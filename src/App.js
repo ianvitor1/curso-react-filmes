c4b5f72399b9ba684587d93362f097a4bcbabafb
ianvitor1 <ianvitoroliverdomin@gmail.com>
2024-10-18 21:48:43 -0300

    new

--- src/App.js
+++ src/App.js
@@ -1,57 +1,46 @@
 import './App.css';
-
-const filmes = {
-  Search: [
-    {
-      Title: 'Homem de Ferro',
-      Year: '2008',
-      Poster: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Iron_Man_poster.jpg',
-    },
-    {
-      Title: 'Rio',
-      Year: '2011',
-      Poster: 'https://diariodorio.com/wp-content/uploads/2011/01/Poster-Rio-11.jpg',
-    },
-    {
-      Title: 'Carros',
-      Year: '2006',
-      Poster: 'https://play-lh.googleusercontent.com/kHt8qbche40zkE6yZwA8IUe5ARINp4PjrJ5mi622GEzQFYWCaGTQDAl58rrRKRKyhnD2=w240-h480-rw',
-    },
-    {
-      Title: 'Sorria',
-      Year: '2022',
-      Poster: 'https://br.web.img3.acsta.net/pictures/22/09/20/15/51/1507365.jpg',
-    },
-    {
-      Title: 'Spirit',
-      Year: '2002',
-      Poster: 'https://br.web.img3.acsta.net/medias/nmedia/18/97/04/04/20504443.jpg',
-    },
-    {
-      Title: 'Missão ataque',
-      Year: '2022',
-      Poster: 'https://m.media-amazon.com/images/S/pv-target-images/c8233bc104d3ed0bf09ea026315037414d617165c580e855e65b6e045d00ecac.jpg',
-    },
-    {
-      Title: 'Adão Negro',
-      Year: '2022',
-      Poster: 'https://upload.wikimedia.org/wikipedia/pt/6/63/Black_Adam_poster.jpg',
-    },
-    {
-      Title: 'The Walking Dead',
-      Year: '2010',
-      Poster: 'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/78/35/82/20303823.jpg',
-    },
-  ]
-}
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [filmes, setFilmes] = useState({ Search: [] });
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const fetchFilmes = async (term) => {
+    try {
+      const response = await
+        fetch(`https://www.omdbapi.com/?apikey=52298090&s=${term}&type=movie&plot=full`);
+      const data = await response.json();
+  
+      if (data.Response === "True") {
+        setFilmes(data);
+      } else {
+        console.error('Erro ao buscar filmes:', data.error);
+      }
+    } catch (error) {
+      console.error('Erro ao fazer a requesição:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFilmes(searchTerm);
+  }, [searchTerm]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    fetchFilmes(searchTerm);
+  };
+  
   return (
     <div className="App">
       <div className='boxFilmes'>
         <h2>Buscar Filmes: </h2>
-        <form>
-          <input placeholder='Digite o nome do filme...'/>
+        <form onSubmit={handleSearch}>
+          <input
+            type='text'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder='Digite o nome do filme...'
+          />  
           <button type='submit'>Buscar</button>
         </form>
         <div className="filmesGrid">
